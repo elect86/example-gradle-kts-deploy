e9c4035b05687ad01c55b4757df4e10a81602330
Giuseppe Barbieri <elect86@gmail.com>
2021-03-04 19:04:48 +0100

    done

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "io.repsy.example"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+
+    repositories {
+        /*
+            this will fetch repsyUsername and repsyPassword from:
+            - gradle.properties file (in your home, it must not be under VCS!)
+            - command line arguments
+            - environment variables
+            - or a combination of those options
+         */
+        maven {
+            name = "repsy"
+            url = uri("https://repo.repsy.io/mvn/MyRepsyUsername/MyRepositoryName")
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
